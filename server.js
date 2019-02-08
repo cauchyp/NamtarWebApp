ede49e8d567ae4976a055b43f54c157c8c915d51
Cauchyzor <pierrot.cauchy@gmail.com>
2019-02-08 18:13:52 +0100

    Ajout des routine pour ouvrire fermer database

--- server.js
+++ server.js
@@ -1,9 +1,29 @@
-var app = require('express')();
+let app = require('express')();
+const sqlite3 = require('sqlite3').verbose();
 
-app.get('/', function(request,response) {
+app.get('/', function (request, response) {
     console.log(request.url);
-    response.render('index.ejs',{paramettreTransmit : 'Je suis un Licornet'});
+    response.render('index.ejs', { paramettreTransmit: 'Je suis un Licornet' });
+
+    // open database in memorys
+    let db = new sqlite3.Database(':memory:', (err) => {
+        if (err) {
+            return console.error(err.message);
+        }
+        console.log('Connected to the in-memory SQlite database.');
+    });
+
+    // close the database connection
+    db.close((err) => {
+        if (err) {
+            return console.error(err.message);
+        }
+        console.log('Close the database connection.');
+    });
 });
+
+
+
 
 app.listen(80);
 console.log('Script Entièrement Chargé');
