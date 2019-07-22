0ffc9ad7061fd3464eb91b34e0eeffd7ccf70164
Cauchy <pierrot.cauchy@cgi.com>
2019-07-22 17:10:40 +0200

    Changing template homepage

--- server.js
+++ server.js
@@ -3,13 +3,17 @@ let app = express();
 const sqlite3 = require('sqlite3').verbose();
 
 
+app.use(express.static(__dirname + '/node_modules'));
 app.use(express.static(__dirname + '/public'));
-app.set('views', __dirname + '/views');
+// app.set('views', __dirname + '/views');
 
 app.get('/', function (request, response) {
     console.log(request.url);
     response.render('index.ejs', { paramettreTransmit: 'Je suis un Licornet' });
 
+});
+
+app.get('/testDataBase', function(request, response){
     // open database in memorys
     let db = new sqlite3.Database(':memory:', (err) => {
         if (err) {
@@ -25,8 +29,7 @@ app.get('/', function (request, response) {
         }
         console.log('Close the database connection.');
     });
-});
-
+})
 
 app.listen(80);
 console.log("application accessible ici : http://localhost:80/");
