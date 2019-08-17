86a753b98fd34c3f455311ea236584237896653f
Cauchyzor <pierrot.cauchy@gmail.com>
2019-08-17 13:24:13 +0200

    generation aptitude liste avec DB

--- server.js
+++ server.js
@@ -31,10 +31,28 @@ app.get('/reglesChap2', function (request, response) {
 
 });
 
-app.get('/fichePerso',function (request, response){
+app.get('/fichePerso', function (request, response) {
     console.log(request.url);
-    response.render('fichePerso.ejs');
+    var db = new sqlite3.Database('spellGenerator.db', (err) => {
+        if (err) {
+            return console.error(err.message);
+        }
+        console.log('Connected to the in-memory SQlite database.');
+    });
+
+    db.all("SELECT * FROM aptitude", function (err, rows) {
+        response.render('fichePerso.ejs', { aptitudeList: rows });
+    });
+
+    // close the database connection
+    db.close((err) => {
+        if (err) {
+            return console.error(err.message);
+        }
+        console.log('Close the database connection.');
+    });
 });
+
 app.get('/generateur', function (request, response) {
     console.log(request.url);
     var effetsExtrait;
@@ -45,10 +63,10 @@ app.get('/generateur', function (request, response) {
         console.log('Connected to the in-memory SQlite database.');
     });
 
-    db.all("SELECT * FROM marque", function(err, rows) {
+    db.all("SELECT * FROM marque", function (err, rows) {
         console.log(rows);
-        response.render('generateur.ejs', { effets : rows});
-	});	
+        response.render('generateur.ejs', { effets: rows });
+    });
 
     // close the database connection
     db.close((err) => {
@@ -58,6 +76,7 @@ app.get('/generateur', function (request, response) {
         console.log('Close the database connection.');
     });
 });
+
 
 app.listen(80);
 console.log("application accessible ici : http://localhost:80/");
