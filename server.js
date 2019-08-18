fad49f4b8b1cfda2e973e43d9f676bbe98cbec2f
Cauchyzor <pierrot.cauchy@gmail.com>
2019-08-18 15:45:57 +0200

    Aptitude table view générée par la base

--- server.js
+++ server.js
@@ -21,8 +21,24 @@ app.get('/histoire', function (request, response) {
 
 app.get('/reglesChap1', function (request, response) {
     console.log(request.url);
-    response.render('reglesChap1.ejs', { paramettreTransmit: 'Je suis un Licornet' });
+    var db = new sqlite3.Database('spellGenerator.db', (err) => {
+        if (err) {
+            return console.error(err.message);
+        }
+        console.log('Connected to the in-memory SQlite database.');
+    });
 
+    db.all("SELECT * FROM aptitude", function (err, rows) {
+        response.render('reglesChap1.ejs', { aptitudeList: rows });
+    });
+
+    // close the database connection
+    db.close((err) => {
+        if (err) {
+            return console.error(err.message);
+        }
+        console.log('Close the database connection.');
+    });
 });
 
 app.get('/reglesChap2', function (request, response) {
